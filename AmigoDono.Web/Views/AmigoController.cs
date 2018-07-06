990de42164331319fcfaf415808fc7a4e4731da0
RaFael Felipe Leite Duarte <rafinha.lduarte@hotmail.com>
2018-07-05 22:17:31 -0300

    commit

---
+++ AmigoDono.Web/Views/AmigoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AmigoDono.Web.Views
+{
+    public class AmigoController : Controller
+    {
+        // GET: Amigo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
