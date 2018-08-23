92ea014d3837e9a776702e7432facdfd0a86549e
RaFael Felipe Leite Duarte <rafinha.lduarte@hotmail.com>
2018-08-23 15:30:55 -0300

    23/08

--- AmigoDono.Web/Controllers/AMIGOController.cs
+++ AmigoDono.Web/Controllers/AMIGOController.cs
@@ -21,27 +21,6 @@ namespace AmigoDono.Web.Views
         }
 
 
-        // GET: AMIGO
-            public ActionResult Index()
-        {
-            return View(_repositoryAmigo.SelecionarTodos());
-        }
-
-        //// GET: AMIGO/Details/5
-        //public ActionResult Details(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    AMIGO aMIGO = db.AMIGO.Find(id);
-        //    if (aMIGO == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(aMIGO);
-        //}
-
         // GET: AMIGO/Create
         public ActionResult Create()
         {
@@ -53,90 +32,29 @@ namespace AmigoDono.Web.Views
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "IDA,Nome,Funcao,Senha,CPF,CNPJ,DataNascimento,DataCadastro,Email,CEP,Logradouro,NomeLogradouro,Numero,Complemento,Bairro,Cidade,UF,Telefone,Celular,Ajuda,Imagem")] AMIGO oAmigo, HttpPostedFileBase imagem, string chkRemoverImagem)
+        public ActionResult Create(AMIGO oAmigo, HttpPostedFileBase imagem, string chkRemoverImagem)
         {
-            try
-            {
+           
                 if (ModelState.IsValid)
-                { if (chkRemoverImagem != null)
-                    {
-                        oAmigo.Imagem = null;
-                    }
+                 { 
+                //    if (chkRemoverImagem != null)
+                //    {
+                //        oAmigo.Imagem = null;
+                //    }
 
-                    if (imagem != null)
-                    {
-                        
-                        oAmigo.Imagem = SetImagem(imagem);
-                    }
+                //    if (imagem != null)
+                //    {                       
+                //        oAmigo.Imagem = SetImagem(imagem);
+                //    }
 
                     _repositoryAmigo.Incluir(oAmigo);
-                    return RedirectToAction("Index");
+                    return RedirectToAction("Index", "Home");
                 }
                
                 return View(oAmigo);
-            }
-            catch
-            {
-                return View(oAmigo);
-            }
+            
+            
         }
-
-        // GET: AMIGO/Edit/5
-        //public ActionResult Edit(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    AMIGO aMIGO = db.AMIGO.Find(id);
-        //    if (aMIGO == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(aMIGO);
-        //}
-
-        //// POST: AMIGO/Edit/5
-        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Edit([Bind(Include = "IDA,Nome,Funcao,Senha,CPF,CNPJ,DataNascimento,DataCadastro,Email,CEP,Logradouro,NomeLogradouro,Numero,Complemento,Bairro,Cidade,UF,Telefone,Celular,Ajuda,Imagem")] AMIGO aMIGO)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Entry(aMIGO).State = EntityState.Modified;
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-        //    return View(aMIGO);
-        //}
-
-        //// GET: AMIGO/Delete/5
-        //public ActionResult Delete(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    AMIGO aMIGO = db.AMIGO.Find(id);
-        //    if (aMIGO == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(aMIGO);
-        //}
-
-        //// POST: AMIGO/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult DeleteConfirmed(int id)
-        //{
-        //    AMIGO aMIGO = db.AMIGO.Find(id);
-        //    db.AMIGO.Remove(aMIGO);
-        //    db.SaveChanges();
-        //    return RedirectToAction("Index");
-        //}
 
         protected override void Dispose(bool disposing)
         {
